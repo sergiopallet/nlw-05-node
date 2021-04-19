0b5198f1f0f8588532ea79e76c2bbb647a0d47dd
Sergio Pallet <sergiopallet@gmail.com>
2021-04-19 19:28:57 -0300

    Seting up project

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({message:"olá"});
+});
+
+
+app.post("/users", (request, response) => {
+    return response.json({message:"usuário salvo com sucesso"});
+});
+
+app.listen(3333, () => console.log("server is running on port 3333"));
